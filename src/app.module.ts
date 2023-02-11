7af57a4b63c68ffeb302b5c5b5fad938c78f69c5
zgibex333 <zgibex333@gmail.com>
2023-02-11 21:23:53 +0300

    prisma + postgres / users rebuilt

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AlbumModule } from './albums/album.module';
 import { ArtistModule } from './artists/artist.module';
 import { DBModule } from './db/db.module';
 import { FavoritesModule } from './favourites/favourites.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { TrackModule } from './tracks/track.module';
 import { UsersModule } from './users/users.module';
 
@@ -14,6 +15,7 @@ import { UsersModule } from './users/users.module';
     DBModule,
     AlbumModule,
     FavoritesModule,
+    PrismaModule,
   ],
 })
 export class AppModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,44 +3,88 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
-import DB from 'src/db/db';
 import { UpdatePasswordDto } from './dto/updatePassword.dto';
 import { CreateUserDTO } from './dto/createUser.dto';
 import { UserEntity } from './users.interface';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly db: DB) {}
+  constructor(
+    private readonly prisma: PrismaService,
+  ) {}
   async findMany() {
-    const users = await this.db.users.findMany();
-
-    return users.map((user) => new UserEntity(user));
+    const users = await this.prisma.user.findMany();
+    return users.map((user) => {
+      const newUser: UserEntity = {
+        ...user,
+        createdAt: user.createdAt.getTime(),
+        updatedAt: user.updatedAt.getTime(),
+      };
+      return new UserEntity(newUser);
+    });
   }
 
   async findOne(id: string) {
-    const user = await this.db.users.findOne({ key: 'id', equals: id });
+    const user = await this.prisma.user.findUnique({ where: { id } });
     if (!user) throw new NotFoundException();
-    return new UserEntity(user);
+    const newUser: UserEntity = {
+      ...user,
+      createdAt: user.createdAt.getTime(),
+      updatedAt: user.updatedAt.getTime(),
+    };
+    return new UserEntity(newUser);
   }
 
   async create(createUserDTO: CreateUserDTO) {
-    const user = await this.db.users.create(createUserDTO);
-    return new UserEntity(user);
+    const user = await this.prisma.user.create({
+      data: createUserDTO,
+    });
+    const newUser: UserEntity = {
+      ...user,
+      createdAt: user.createdAt.getTime(),
+      updatedAt: user.updatedAt.getTime(),
+    };
+    return new UserEntity(newUser);
   }
 
   async delete(id: string) {
-    const user = await this.db.users.findOne({ key: 'id', equals: id });
+    const user = await this.prisma.user.findUnique({ where: { id } });
     if (!user) throw new NotFoundException();
-    await this.db.users.delete(id);
-    return new UserEntity(user);
+    const deleteduser = await this.prisma.user.delete({ where: { id } });
+    const newUser: UserEntity = {
+      ...deleteduser,
+      createdAt: deleteduser.createdAt.getTime(),
+      updatedAt: deleteduser.updatedAt.getTime(),
+    };
+    return new UserEntity(newUser);
   }
 
   async change(id: string, passwords: UpdatePasswordDto) {
-    const user = await this.db.users.findOne({ key: 'id', equals: id });
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
     if (!user) throw new NotFoundException();
     const { oldPassword, newPassword } = passwords;
     if (oldPassword !== user.password) throw new ForbiddenException();
-    const newUser = await this.db.users.change(id, { password: newPassword });
+    const updatedUser = await this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        password: newPassword,
+        version: {
+          increment: 1
+        }
+      },
+    });
+    const newUser: UserEntity = {
+      ...updatedUser,
+      createdAt: updatedUser.createdAt.getTime(),
+      updatedAt: updatedUser.updatedAt.getTime(),
+    };
     return new UserEntity(newUser);
   }
 }
