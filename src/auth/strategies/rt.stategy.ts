e15446f327656bee40f88aa0ec0f84c5b90d7adf
Dzianis Chaplia <d.chaplia@godeltech.com>
2023-02-25 20:17:57 +0100

    update tests according to rs-school message in announcements

--- src/auth/strategies/rt.stategy.ts
+++ src/auth/strategies/rt.stategy.ts
@@ -4,7 +4,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { Request } from 'express';
 
 @Injectable()
-export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
+export class RtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
